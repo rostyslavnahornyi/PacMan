c1f6885c38ed89c93f8b163381ccd6a38ed8daa2
rostyslavnahornyi <rostyslav.nahornyi@nure.ua>
2021-06-25 08:58:22 +0300

    added enums for directions

--- Entities/Ghost.cs
+++ Entities/Ghost.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace PacMan_GUI_WPF
 {
+    enum Direction
+    {
+        LEFT,
+        UP,
+        RIGHT,
+        DOWN,
+    }
     class Ghost : Entity
     {
         public int x;
@@ -29,13 +36,13 @@ namespace PacMan_GUI_WPF
         {
             string toGo = null;
 
-            Entity[] neighbourCells = { Field.entitiesArr[x - 1, y], Field.entitiesArr[x + 1, y], Field.entitiesArr[x, y - 1], Field.entitiesArr[x, y + 1] };
+            Entity[] neighbourCells = { Field.entitiesArr[x - 1, y], Field.entitiesArr[x, y - 1], Field.entitiesArr[x + 1, y], Field.entitiesArr[x, y + 1] };
             List<string> possibleDirections = new List<string>();
             for (int i = 0; i < neighbourCells.Length; i++)
             {
                 if (neighbourCells[i].Passability == true) 
                 {
-                    if (i == 0 && direction != "RIGHT")
+                    /*if (i == 0 && direction != "RIGHT")
                     {
                         possibleDirections.Add("LEFT");
                     }
@@ -50,6 +57,10 @@ namespace PacMan_GUI_WPF
                     if (i == 3 && direction != "UP")
                     {
                         possibleDirections.Add("DOWN");
+                    }*/                    
+                    if (direction != (((Direction)(Math.Abs(i - 2))).ToString()))
+                    {
+                        possibleDirections.Add(((Direction)i).ToString());
                     }
                 }
             }
@@ -60,63 +71,6 @@ namespace PacMan_GUI_WPF
             {
                 toGo = possibleDirections[Util.random.Next(possibleDirections.Count)];
             }
-
-
-            //string[] possibleDirections = new string[2];
-            //int possibleDirectionsIndex = 0;
-
-            //if (neighbourCells[0] != Constants.Wall && neighbourCells[0] != Constants.Ghost && direction != "RIGHT")
-            //{
-            //    possibleDirections[possibleDirectionsIndex] = "LEFT";
-            //    possibleDirectionsIndex++;
-            //}
-            //if (neighbourCells[1] != Constants.Wall && neighbourCells[1] != Constants.Ghost && direction != "LEFT")
-            //{
-            //    possibleDirections[possibleDirectionsIndex] = "RIGHT";
-            //    possibleDirectionsIndex++;
-            //}
-            //if (neighbourCells[2] != Constants.Wall && neighbourCells[2] != Constants.Ghost && direction != "DOWN")
-            //{
-            //    possibleDirections[possibleDirectionsIndex] = "UP";
-            //    possibleDirectionsIndex++;
-            //}
-            //if (neighbourCells[3] != Constants.Wall && neighbourCells[3] != Constants.Ghost && direction != "UP")
-            //{
-            //    possibleDirections[possibleDirectionsIndex] = "DOWN";
-            //}
-
-            //if (possibleDirections[0] == null && possibleDirections[1] == null)
-            //{
-            //    if ((neighbourCells[0] != Constants.Wall || neighbourCells[0] == Constants.PacMan) && neighbourCells[0] != Constants.Ghost && direction == "RIGHT")
-            //    {
-            //        toGo = "LEFT";
-            //    }
-            //    if ((neighbourCells[1] != Constants.Wall || neighbourCells[1] == Constants.PacMan) && neighbourCells[1] != Constants.Ghost && direction == "LEFT")
-            //    {
-            //        toGo = "RIGHT";
-            //    }
-            //    if ((neighbourCells[2] != Constants.Wall || neighbourCells[2] == Constants.PacMan) && neighbourCells[2] != Constants.Ghost && direction == "DOWN")
-            //    {
-            //        toGo = "UP";
-            //    }
-            //    if ((neighbourCells[3] != Constants.Wall || neighbourCells[3] == Constants.PacMan) && neighbourCells[3] != Constants.Ghost && direction == "UP")
-            //    {
-            //        toGo = "DOWN";
-            //    }
-            //}
-            //else if (possibleDirections[0] == null)
-            //{
-            //    toGo = possibleDirections[1];
-            //}
-            //else if (possibleDirections[1] == null)
-            //{
-            //    toGo = possibleDirections[0];
-            //}
-            //else
-            //{
-            //    toGo = possibleDirections[Util.random.Next(2)];
-            //}
-
             return toGo;
         }
     }
